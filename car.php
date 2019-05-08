1145f1eb497727b45a137cd37870f2bb18eaca2d
Mark Rasheed <azizconqueror1@gmail.com>
2019-05-08 20:18:54 +0100

    the php file to select the car and its color

---
+++ car.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+
+// get the form input 
+
+$type = $_POST['selType'];
+$color = $_POST['txtColor'];
+
+
+
+
+//  use the form input 
+echo "Your $color  $type   is ready ....safe driving!!";
+ ?>
+
+
+
+
